6cdcf47d725fd3f7538b8b3be697e0cee6aeef70
Ayan-Khare <142321492+Ayan-Khare@users.noreply.github.com>
2025-03-15 15:44:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Install system dependencies
-apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
-
-apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+apt-get update && apt-get install libgl1
 
 # Start the server
 uvicorn api.main:app --host 0.0.0.0 --port 8000
