673c4287db037071e15753ccf68b117088ea7492
Ayan-Khare <142321492+Ayan-Khare@users.noreply.github.com>
2025-03-15 14:45:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 # Install missing OpenGL and OpenCV dependencies
 apt-get update && apt-get install -y libgl1-mesa-glx
 
+pip install -r requirements.txt
+
 # Run the FastAPI server
 uvicorn api.main:app --host 0.0.0.0 --port 8000
