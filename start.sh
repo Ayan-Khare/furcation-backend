1d368abbda0ec4d83997cd366f0f3a30e3910acf
Ayan-Khare <142321492+Ayan-Khare@users.noreply.github.com>
2025-03-15 16:05:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Install system dependencies
-apt-get update && apt-get install libgl1 && apt-get install -y python3-opencv
+apt-get update && apt-get install libgl1 
+#!/bin/bash
+pip uninstall -y opencv-python
+pip install --no-cache-dir opencv-python-headless
+uvicorn api.main:app --host 0.0.0.0 --port $PORT
 
-# Start the server
-uvicorn api.main:app --host 0.0.0.0 --port 8000
